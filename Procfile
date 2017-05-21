f0990e9da050752eb806f09ce56fff1f22572a64
Mitch VanDuyn <mitch@catprint.com>
2017-05-21 03:52:32 +0000

    initial commit'

---
+++ Procfile
@@ -0,0 +1,3 @@
+web:        bundle exec rails s -p 8080
+hot-loader: bundle exec opal-hot-reloader -d app/hyperloop -p 8081
+
---
+++ config/initializers/hyperloop.rb
@@ -0,0 +1,6 @@
+# config/initializers/hyperloop.rb        
+Hyperloop.configuration do |config|
+  config.transport = :action_cable
+  config.compress_system_assets = false # its not worth minimizing assets cause cloud9 is slow
+  config.import 'opal_hot_reloader' if Rails.env.development?
+end
