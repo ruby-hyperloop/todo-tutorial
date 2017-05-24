89fd95770f37cb776745ff2648914cbe52216bc4
Mitch VanDuyn <mitch@catprint.com>
2017-05-24 19:54:04 +0000

    made app more interesting

--- app/hyperloop/components/app.rb
+++ app/hyperloop/components/app.rb
@@ -4,9 +4,13 @@ class App < Hyperloop::Router
   
   history :browser
   
+  after_mount do
+    every(1) { force_update! }
+  end
+  
   route do
     
-    SPAN { 'Hyperloop at your service!' }
+    SPAN { "Hyperloop at your service! - The time is #{Time.now}" }
     
     # add routes anywhere in this block... any route matching will be displayed
     # here are some samples (note you can mix Routes with other components)
