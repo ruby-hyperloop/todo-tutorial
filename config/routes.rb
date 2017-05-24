e2ec03b505eaaf462f6096be623c89ac81440802
Mitch VanDuyn <mitch@catprint.com>
2017-05-24 11:45:57 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount Hyperloop::Engine => '/hyperloop'
-  root 'hyperloop#app'
+  # root 'hyperloop#app' # route just the root to the component named App
+  get '/(*other)', to: 'hyperloop#app' # route everything to the App component
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
