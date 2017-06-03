7aca25c3b694212310bb18c280d050cfa74799a3
Mitch VanDuyn <mitch@catprint.com>
2017-06-03 12:16:55 -0400

    prererendering off by default if on cloud9

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -1,5 +1,6 @@
 # config/initializers/hyperloop.rb        
 Hyperloop.configuration do |config|
+  config.prerendering = :off if ENV['C9_PROJECT']
   config.transport = :action_cable
   config.compress_system_assets = false # its not worth minimizing assets cause cloud9 is slow
   config.import 'opal_hot_reloader' if Rails.env.development?
