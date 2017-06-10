4033a851338f83f826782ee257b2908306f9cbee
Mitch VanDuyn <mitch@catprint.com>
2017-06-10 15:03:22 +0000

    better application_record handling

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -1,7 +1,12 @@
 # config/initializers/hyperloop.rb        
 Hyperloop.configuration do |config|
-  config.prerendering = :off if ENV['C9_PROJECT']
+  if ENV['C9_PROJECT']
+    # Cloud 9 base workspaces are only 512MB which is too small effectively support these features
+    # if you increase your cloud9 workspace to 1GB you can remove this
+    config.prerendering = :off
+    config.compress_system_assets = false
+  end
   config.transport = :action_cable
-  config.compress_system_assets = false # its not worth minimizing assets cause cloud9 is slow
   config.import 'opal_hot_reloader' if Rails.env.development?
+  config.import 'models/application_record' # force application_record to load first
 end
